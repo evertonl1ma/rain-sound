3d55643640ea9ff9e3b9fe19e763dad78ead9a8c
Everton Lima <ever-tonlima@hotmail.com>
2016-05-28 10:55:37 -0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,22 @@
+
+   		 audio= document.getElementById("audio");
+         volume= document.getElementById("volume");
+
+			function aumentar_volume(){
+				if( audio.volume < 1)  audio.volume += 0.1;
+				console.log(audio.volume);
+			}
+
+			function diminuir_volume(){
+				if( audio.volume > 0)  audio.volume -= 0.1;
+			}
+			
+			function mute(){
+				if(audio.muted){
+					volume.src= "img/volume-3.png";
+					audio.muted = false;
+				} else{
+					audio.muted = true;
+					volume.src= "img/volume-1.png";
+				}
+			}
